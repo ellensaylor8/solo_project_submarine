40ca05d85be4edf7b08a70c3751d91d287600ed6
Ellen Saylor <ellensaylor8@gmail.com>
2019-09-11 14:52:47 -0500

    created environment variable

---
+++ build/precache-manifest.0dae3d21175e204bcbbfb6279ccc6506.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1066c910fa18a1c0f97144b64fe1a144",
+    "url": "/index.html"
+  },
+  {
+    "revision": "160a9825a1f4a334ff34",
+    "url": "/static/css/main.c079c376.chunk.css"
+  },
+  {
+    "revision": "9e3dcfaea513c215938d",
+    "url": "/static/js/2.a15b89e1.chunk.js"
+  },
+  {
+    "revision": "160a9825a1f4a334ff34",
+    "url": "/static/js/main.53c107e7.chunk.js"
+  },
+  {
+    "revision": "0cc1af7f0a72b65a42aa",
+    "url": "/static/js/runtime~main.4f3a269a.js"
+  },
+  {
+    "revision": "7ce9680e738565539c06cf0326d7302f",
+    "url": "/static/media/submarine.7ce9680e.svg"
+  }
+]);
